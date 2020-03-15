2dedaa4f2f57d3923ed6265a4968e1a31eb0ef0e
rohan <rshanker779@gmail.com>
2020-03-15 20:32:56 +0000

    Adding directed and cycles

--- graphs.py
+++ graphs.py
@@ -1,7 +1,10 @@
-from typing import List, Set, Iterable, Tuple
+import operator
+from functools import reduce
+from typing import List, Set, Iterable, Tuple, Optional, FrozenSet
 import matplotlib.pyplot as plt
 import numpy as np
 import pytest
+from more_itertools import flatten
 
 from mixin import StringMixin
 
@@ -22,32 +25,54 @@ class Node(StringMixin):
         return self.id == other.id
 
 
-class Link:
-    def __init__(self, node_1: Node, node_2: Node):
+class Link(StringMixin):
+    def __init__(self, node_1: Node, node_2: Node, is_directed: bool = False):
+        super().__init__()
         self.node_1 = node_1
         self.node_2 = node_2
+        self.is_directed = is_directed
 
     def __hash__(self):
-        return hash((self.node_1, self.node_2))
+        return hash((self.node_1, self.node_2, self.is_directed))
 
     def __eq__(self, other):
         if not isinstance(other, self.__class__):
             return NotImplemented
-        return (self.node_1, self.node_2) == (other.node_1, other.node_2)
+        return (self.node_1, self.node_2, self.is_directed) == (
+            other.node_1,
+            other.node_2,
+            other.is_directed,
+        )
+
+    @property
+    def nodes(self) -> List[Node]:
+        return [self.node_1, self.node_2]
+
+
+class PathLink(Link):
+    def __init__(self, node_1, node_2, underlying_link):
+        super().__init__(node_1, node_2, True)
+        self.underlying_link = underlying_link
 
 
 class PathReport:
-    def __init__(self, from_node: Node, to_node: Node, paths: List[Tuple[Node, ...]]):
+    def __init__(
+        self, from_node: Node, to_node: Node, paths: List[Tuple[PathLink, ...]]
+    ):
         self.from_node = from_node
         self.to_node = to_node
         self.is_possible = len(paths) > 0
         self.paths = paths
-        self.distance = min(len(p) for p in paths) - 1 if paths else -1
+        self.distance = min(len(p) for p in paths) if paths else -1
 
 
 class Graph:
     def __init__(self, nodes: List[Node], links: List[Link]):
         self.nodes = set(nodes)
+        assert all(i.is_directed for i in links) or all(
+            not i.is_directed for i in links
+        )
+        self.is_directed = next(i.is_directed for i in links)
         self.links = set(links)
 
     def plot_graph(self):
@@ -61,7 +86,21 @@ class Graph:
         )
         for link in self.links:
             points = [node_map[link.node_1.id], node_map[link.node_2.id]]
-            plt.plot([np.real(i) for i in points], [np.imag(i) for i in points], c="b")
+            x = [np.real(i) for i in points]
+            y = [np.imag(i) for i in points]
+            plt.plot(x, y, c="b")
+            if link.is_directed:
+                eps = 0.02
+                plt.arrow(
+                    x[-1],
+                    y[-1],
+                    eps * (x[-1] - x[0]),
+                    eps * (y[-1] - y[0]),
+                    shape="full",
+                    lw=0,
+                    length_includes_head=True,
+                    head_width=0.05,
+                )
         plt.show()
 
     @property
@@ -72,6 +111,14 @@ class Graph:
     def size(self) -> int:
         return len(self.links)
 
+    def find_link(self, node_1, node_2) -> Optional[Link]:
+        forward_link = Link(node_1, node_2, self.is_directed)
+        if forward_link in self.links:
+            return forward_link
+        backwards_link = Link(node_2, node_1)
+        if backwards_link in self.links:
+            return backwards_link
+
     def get_roots_of_unity(self, k, n) -> complex:
         return np.exp(2 * np.pi * 1j * k / n)
 
@@ -79,10 +126,15 @@ class Graph:
         return node in self.nodes
 
     def are_neighbours(self, node_1: Node, node_2: Node) -> bool:
-        return Link(node_1, node_2) in self.links or Link(node_2, node_1) in self.links
+        """Returns if there is a length one path between node_1 and node_2"""
+        return self.find_link(node_1, node_2) is not None
 
     def get_neighbourhood(self, node: Node) -> Set[Node]:
-        matching_links = {l for l in self.links if l.node_1 == node or l.node_2 == node}
+        matching_links = [
+            self.find_link(node, n)
+            for n in self.nodes
+            if self.find_link(node, n) is not None
+        ]
         return (
             {l.node_1 for l in matching_links} | {l.node_2 for l in matching_links}
         ) - {node}
@@ -105,27 +157,44 @@ class Graph:
     def is_k_regular(self, k: int) -> bool:
         return set(self.degree_sequence) == {k}
 
-    def get_connections(self, node_1: Node, node_2: Node) -> PathReport:
+    def get_paths(self, node_1: Node, node_2: Node) -> PathReport:
 
-        paths = [(node_1,)]
+        paths: List[Tuple[PathLink, ...]] = []
+        for node in self.get_neighbourhood(node_1):
+            link = self.find_link(node_1, node)
+            if link is not None:
+                paths.append((PathLink(node_1, node, link),))
         # We traverse breadth first to expand to all possible (acyclic) paths visiting unique points
         # Note if we have Link(1,2) and Link(2,1), we would still only get the path 1,2.
         # This is more about connectivity then full path enumeration
         while True:
             next_paths = paths
-            for *initial, p in paths:
-                next_steps = self.get_neighbourhood(p) - set(initial)
-                next_paths += [tuple([*initial, p, i]) for i in next_steps]
+            for *initial, last_link in paths:
+                last_node = last_link.node_2
+                next_steps = self.get_neighbourhood(last_node) - set(
+                    flatten(i.nodes for i in initial)
+                )
+                for step in next_steps:
+                    link = self.find_link(last_node, step)
+                    if (
+                        link is not None
+                        and link not in {i.underlying_link for i in initial}
+                        and link != last_link.underlying_link
+                    ):
+                        next_paths.append(
+                            tuple(
+                                [*initial, last_link, PathLink(last_node, step, link)]
+                            )
+                        )
             if len(next_paths) == len(paths):
                 paths = next_paths
                 break
             paths = next_paths
             print(paths)
-        paths_between = [i for i in paths if i[-1] == node_2]
+        paths_between = [i for i in paths if i[-1].node_2 == node_2]
         return PathReport(node_1, node_2, paths_between)
 
     def get_connected_component(self, node: Node) -> Set[Node]:
-
         neighbourhood = self.get_neighbourhood(node) | {node}
         processed_nodes = {node}
         while neighbourhood != set(processed_nodes):
@@ -135,16 +204,46 @@ class Graph:
         return neighbourhood
 
     @property
-    def connected_components(self) -> List[Set[Node]]:
+    def connected_components(self) -> Set[FrozenSet[Node]]:
         processed_nodes = set()
-        components = []
+        components = set()
         while processed_nodes != self.nodes:
             next_node = next(i for i in self.nodes - processed_nodes)
             component = self.get_connected_component(next_node)
-            components.append(component)
+            components.add(frozenset(component))
             processed_nodes |= component
-        return components
+        if not self.is_directed:
+            return components
+
+        clean_components = set()
+        while True:
+            previous_length = len(clean_components)
+            for node in self.nodes:
+                nodes_components = reduce(
+                    operator.or_, [i for i in components if node in i], frozenset()
+                )
+                clean_components.add(nodes_components)
+            if len(clean_components) != previous_length:
+                break
+        return clean_components
 
     @property
     def is_connected(self):
         return len(self.connected_components) == 1
+
+    @property
+    def is_cyclic(self):
+        for starting_node in self.nodes:
+            for next_node in self.nodes - {starting_node}:
+                connections = self.get_paths(starting_node, next_node)
+                for path in connections.paths:
+                    last_link = path[-1]
+                    underlying_links = {i.underlying_link for i in path}
+                    next_link = self.find_link(last_link.node_2, starting_node)
+                    if next_link is not None and next_link not in underlying_links:
+                        return True
+        return False
+
+    @property
+    def is_dag(self):
+        return self.is_directed and not self.is_cyclic
--- test_graphs.py
+++ test_graphs.py
@@ -1,5 +1,5 @@
 import pytest
-from graphs.graphs import Node, Link, Graph
+from graphs.graphs import Node, Link, Graph, PathLink
 from itertools import combinations
 
 
@@ -36,6 +36,23 @@ def disconnected_links(nodes):
 
 
 @pytest.fixture
+def directed_links(nodes):
+    n1, n2, n3, n4, n5 = nodes
+    return [
+        Link(n1, n2, True),
+        Link(n2, n3, True),
+        Link(n1, n4, True),
+        Link(n4, n5, True),
+    ]
+
+
+@pytest.fixture
+def cyclic_directed_links(nodes):
+    n1, n2, n3, n4, n5 = nodes
+    return [Link(n1, n2, True), Link(n2, n3, True), Link(n3, n1, True)]
+
+
+@pytest.fixture
 def line_graph(nodes, line_links):
     return Graph(nodes, line_links)
 
@@ -48,6 +65,16 @@ def complete_graph(nodes, complete_links):
 @pytest.fixture
 def disconnected_graph(nodes, disconnected_links):
     return Graph(nodes, disconnected_links)
+
+
+@pytest.fixture
+def directed_graph(nodes, directed_links):
+    return Graph(nodes, directed_links)
+
+
+@pytest.fixture
+def cyclic_directed_graph(nodes, cyclic_directed_links):
+    return Graph(nodes, cyclic_directed_links)
 
 
 def test_line_graph(nodes, line_links, line_graph):
@@ -68,23 +95,24 @@ def test_line_graph(nodes, line_links, line_graph):
     assert line_graph.minimum_degree == 1
     assert not line_graph.is_k_regular(1)
     for i, j in combinations(nodes, 2):
-        path = line_graph.get_connections(i, j)
+        path = line_graph.get_paths(i, j)
         assert path.is_possible
 
-    path = line_graph.get_connections(n1, n5)
-    assert path.paths == [(n1, n2, n3, n4, n5)]
+    path = line_graph.get_paths(n1, n5)
+    assert len(path.paths) == 1
     assert path.distance == 4
-    path = line_graph.get_connections(n2, n4)
-    assert path.paths == [(n2, n3, n4)]
+    path = line_graph.get_paths(n2, n4)
+    assert len(path.paths) == 1
     assert path.distance == 2
-    path = line_graph.get_connections(n5, n4)
-    assert path.paths == [(n5, n4)]
+    path = line_graph.get_paths(n5, n4)
+    assert len(path.paths) == 1
     assert path.distance == 1
     for node in nodes:
         assert line_graph.get_connected_component(node) == set(nodes)
         assert node in line_graph.nodes
     assert set(nodes) in line_graph.connected_components
     assert line_graph.is_connected
+    assert not line_graph.is_cyclic
 
 
 def test_complete_graph(complete_graph, nodes):
@@ -95,7 +123,7 @@ def test_complete_graph(complete_graph, nodes):
         assert complete_graph.are_neighbours(first, second)
         assert complete_graph.get_neighbourhood(first) == set(nodes) - {first}
         assert complete_graph.get_degree(first) == 4
-        assert complete_graph.get_connections(first, second).is_possible
+        assert complete_graph.get_paths(first, second).is_possible
     assert set(complete_graph.degree_sequence) == {4}
     assert complete_graph.maximum_degree == 4
     assert complete_graph.minimum_degree == 4
@@ -107,6 +135,7 @@ def test_complete_graph(complete_graph, nodes):
         assert node in complete_graph.nodes
     assert set(nodes) in complete_graph.connected_components
     assert complete_graph.is_connected
+    assert complete_graph.is_cyclic
 
 
 def test_disconnected_graph(disconnected_graph, nodes):
@@ -126,17 +155,17 @@ def test_disconnected_graph(disconnected_graph, nodes):
     assert disconnected_graph.maximum_degree == 1
     assert disconnected_graph.minimum_degree == 0
     assert not disconnected_graph.is_k_regular(1)
-    assert disconnected_graph.get_connections(n1, n2).is_possible
-    assert disconnected_graph.get_connections(n3, n4).is_possible
-    assert not disconnected_graph.get_connections(n1, n3).is_possible
-    assert not disconnected_graph.get_connections(n1, n4).is_possible
-    assert not disconnected_graph.get_connections(n2, n3).is_possible
-    assert not disconnected_graph.get_connections(n2, n4).is_possible
+    assert disconnected_graph.get_paths(n1, n2).is_possible
+    assert disconnected_graph.get_paths(n3, n4).is_possible
+    assert not disconnected_graph.get_paths(n1, n3).is_possible
+    assert not disconnected_graph.get_paths(n1, n4).is_possible
+    assert not disconnected_graph.get_paths(n2, n3).is_possible
+    assert not disconnected_graph.get_paths(n2, n4).is_possible
 
-    path = disconnected_graph.get_connections(n1, n2)
-    assert path.paths == [(n1, n2)]
+    path = disconnected_graph.get_paths(n1, n2)
+    assert path.paths == [(PathLink(n1, n2, Link(n1, n2)),)]
     assert path.distance == 1
-    path = disconnected_graph.get_connections(n2, n4)
+    path = disconnected_graph.get_paths(n2, n4)
     assert path.paths == []
     assert disconnected_graph.get_connected_component(n1) == {n1, n2}
     assert disconnected_graph.get_connected_component(n3) == {n3, n4}
@@ -144,3 +173,43 @@ def test_disconnected_graph(disconnected_graph, nodes):
     assert {n3, n4} in disconnected_graph.connected_components
     assert {n5} in disconnected_graph.connected_components
     assert not disconnected_graph.is_connected
+    assert not disconnected_graph.is_cyclic
+
+
+def test_directed_graph(directed_graph, nodes):
+
+    assert directed_graph.order == 5
+    assert directed_graph.size == 4
+
+    n1, n2, n3, n4, n5 = nodes
+
+    assert directed_graph.are_neighbours(n1, n2)
+    assert not directed_graph.are_neighbours(n2, n1)
+    assert directed_graph.are_neighbours(n4, n5)
+    assert not directed_graph.are_neighbours(n4, n1)
+    assert directed_graph.get_neighbourhood(n1) == {n2, n4}
+    assert directed_graph.get_neighbourhood(n2) == {n3}
+    assert directed_graph.get_degree(n1) == 2
+    assert sorted(directed_graph.degree_sequence) == [0, 0, 1, 1, 2]
+    assert directed_graph.maximum_degree == 2
+    assert directed_graph.minimum_degree == 0
+    assert directed_graph.get_paths(n1, n2).is_possible
+    assert not directed_graph.get_paths(n2, n1).is_possible
+    assert directed_graph.get_paths(n4, n5).is_possible
+    assert not directed_graph.get_paths(n5, n4).is_possible
+
+    path = directed_graph.get_paths(n1, n2)
+    assert path.paths == [(PathLink(n1, n2, Link(n1, n2, True)),)]
+    assert path.distance == 1
+    path = directed_graph.get_paths(n2, n1)
+    assert path.paths == []
+    assert directed_graph.get_connected_component(n1) == set(nodes)
+    assert directed_graph.is_connected
+    assert not directed_graph.is_cyclic
+    assert directed_graph.is_dag
+
+
+def test_directed_acyclic_graph(nodes, cyclic_directed_graph):
+    cyclic_directed_graph.plot_graph()
+    assert cyclic_directed_graph.is_cyclic
+    assert not cyclic_directed_graph.is_dag
